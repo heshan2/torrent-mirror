03e58dcbef5dbf9d3189506509089863c59a00c5
heshan2 <87757215+heshan2@users.noreply.github.com>
2021-07-23 11:31:08 +0530

    Update mirror_status.py

--- bot/modules/mirror_status.py
+++ bot/modules/mirror_status.py
@@ -10,7 +10,7 @@ import threading
 def mirror_status(update, context):
     with download_dict_lock:
         if len(download_dict) == 0:
-            message = "No active downloads"
+            message = "No active downloads(Download Anything)"
             reply_message = sendMessage(message, context.bot, update)
             threading.Thread(target=auto_delete_message, args=(bot, update.message, reply_message)).start()
             return
